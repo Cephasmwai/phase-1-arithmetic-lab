54bca24f6774435af2e5c65e05b3a66653d2943c
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-06 10:47:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31; 
+let num2 = 2;  
+let multiply = num1 * num2; 
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 1000) + 1;
+
+let num3 = 14; 
+let num4 = 5;  
+
+let mod = num3 % num4; 
+
+let numbers = [10, 5, 20, 15]; 
+let max = Math.max(...numbers); 
+
+console.log(max); 
